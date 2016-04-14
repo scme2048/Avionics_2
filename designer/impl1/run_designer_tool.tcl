74efbbaab22caa926b613133c7512b886c7c03bd
scme2048 <scott.mende@colorado.edu>
2016-04-13 22:07:16 -0600

    symposium flash. Only works with logic analyzer hooked up.

--- designer/impl1/run_designer_tool.tcl
+++ designer/impl1/run_designer_tool.tcl
@@ -1,4 +1,8 @@
-set_defvar "IDE_DESIGNERVIEW_ROOTDIR" {C:\Users\Scott\Documents\Docs\School\Senior\Projects\Libero\Avionics_2\designer}
-open_design full_system.adb
-open_tool mvn_ioedit
+# (OPEN DESIGN)
+
+open_design "C:/Users/Scott/Documents/Docs/School/Senior/Projects/Libero/Avionics_2/designer/impl1/full_system.adb"
+export -format "PDB" \
+    -feature "prog_fpga"  \
+    {C:/Users/Scott/Documents/Docs/School/Senior/Projects/Libero/Avionics_2/designer/impl1/full_system.pdb}
+save_design "C:/Users/Scott/Documents/Docs/School/Senior/Projects/Libero/Avionics_2/designer/impl1/full_system.adb"
 
